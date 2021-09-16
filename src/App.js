744b6f6811f826ce543f0dad13fed7a4adbdf5e8
Djaypm89 <djaypm89@gmail.com>
2021-09-16 17:49:25 -0400

    init

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+import React, {Component} from 'react';
+
+
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+
+    }
+  }
+
+  render(){
+    return (
+      <div className="App">
+      <h1>Hello World</h1>
+      </div>
+    ) ;
+  }
+}
+
+export default App;
