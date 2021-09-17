43dae5cb48793c569355efad9204e8245e86e7ad
Djaypm89 <djaypm89@gmail.com>
2021-09-17 17:55:13 -0400

    video display function created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
-import React, {Component} from 'react';
+import React, {Component, useEffect, useState} from 'react';
+// additional imports needed
 
 
-class App extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-
-    }
+function App(props) {
+  const [videos, setVideos] = useState([]);
+  const [filteredVideo, filtersVideos] = useState(); //search variable inside()?
+  
+  async function fetchSearchResults() {
+    let response = await axios.get(`https://youtube.googleapis.com/youtube/v3/search?${filteredVideo}`)  // update url
+    setVideos(response.data.results);
   }
 
+  function mapVidoes(){
+    return videos.map(video =>
+    <Video
+      key={video.videoID}
+      video={video}
+      />
+      )
+  }
+
+useEffect(() => {
+  let mounted = true;
+  if(mounted){
+    fetchVideos();
+  }
+  return () => mounted = false;
+}, [])
 
 
-  render(){
-    return (
-      <div className="App">
-      <h1>Hello World</h1>
+// import video player functional component to this?
+
+
+return (
+  <div>
+    <SongTable mapSongs={()} => mapSongs()} />
       </div>
-    ) ;
+    );
   }
-}
+
 
 export default App;
---
+++ src/components/video.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Video = (props) => {
+    return (
+        <iFrame>
+            {/* html goes here */}
+        </iFrame> 
+    );
+}
+
+export default Video;
